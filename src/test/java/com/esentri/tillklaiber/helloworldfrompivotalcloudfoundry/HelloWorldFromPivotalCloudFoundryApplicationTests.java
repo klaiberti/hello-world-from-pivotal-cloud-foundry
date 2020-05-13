5673c460f57f751453087ff04bf629fd3787bb50
Till Klaiber <till.klaiber@esentri.com>
2020-05-13 10:44:30 +0200

    Initial Commit

---
+++ src/test/java/com/esentri/tillklaiber/helloworldfrompivotalcloudfoundry/HelloWorldFromPivotalCloudFoundryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esentri.tillklaiber.helloworldfrompivotalcloudfoundry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldFromPivotalCloudFoundryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
